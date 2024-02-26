d5a8735979adbadf47a0365547382628ff00ac28
fayadchowdhury <fayad.chowdhury@gmail.com>
2024-02-26 14:15:22 -0800

    Add basic priority queue data structure and common methods.

--- complex.py
+++ complex.py
@@ -1,8 +1,13 @@
 # Import double-ended queue
 from collections import deque
 
-# Implement queue data structure
+# Import heapq module for priority queue
+import heapq
 
+MAX = 0
+MIN = 1
+
+# Implement queue data structure
 class Queue():
     def __init__(self):
         self._elements = deque()
@@ -32,6 +37,7 @@ class Queue():
         else:
             print("No item in queue :(")
 
+# Implement stack data structure
 class Stack():
     def __init__(self):
         self._elements = deque()
@@ -64,6 +70,31 @@ class Stack():
         else:
             print("No item in stack :(")
 
+# Implement priority queue data structure
+class PriorityQueue():
+    def __init__(self, heap_type):
+        self._elements = []
+        self._type = heap_type
+        
+    def __len__(self):
+        return len(self._elements)
+    
+    def empty(self):
+        return len(self._elements) == 0
+        
+    def enqueue(self, priority, value):
+        if self._type == MAX:
+            priority = -priority
+        elif self._type == MIN:
+            priority = priority
+        heapq.heappush(self._elements, (priority, value))
+        
+    def dequeue(self):
+        if len(self._elements) > 0:
+            return heapq.heappop(self._elements)[-1]
+        else:
+            print("No element in queue :(")
+    
 def main():
     # queue = Queue()
     # queue.enqueue(1)
@@ -74,18 +105,34 @@ def main():
     # print(queue.dequeue())
     # print(queue.front())
     
-    s = Stack()
-    print(s.empty()) # True
-    s.push(1)
-    s.push(2)
-    s.push(3)
-    print(s.empty()) # False
-    print(len(s))
-    for elem in s:
-        print(elem)
-    print(s.pop())
-    print(s.pop())
-    print(s.pop())
-    print(s.pop())    
+    # s = Stack()
+    # print(s.empty()) # True
+    # s.push(1)
+    # s.push(2)
+    # s.push(3)
+    # print(s.empty()) # False
+    # print(len(s))
+    # for elem in s:
+    #     print(elem)
+    # print(s.pop())
+    # print(s.pop())
+    # print(s.pop())
+    # print(s.pop())
     
+    IMPORTANT = 2
+    NEUTRAL = 1
+    CRITICAL = 3
+    
+    messages = PriorityQueue(MAX)
+    messages.enqueue(IMPORTANT, "Windshield wipers turned on")
+    messages.enqueue(NEUTRAL, "Radio station tuned in")
+    messages.enqueue(CRITICAL, "Brake pedal depressed")
+    messages.enqueue(IMPORTANT, "Hazard lights turned on")
+    
+    print(messages.dequeue())
+    print(messages.dequeue())
+    print(messages.dequeue())
+    print(messages.dequeue())
+    print(messages.dequeue())
+        
 main()
